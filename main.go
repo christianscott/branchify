caa3345d233017f26b0534ded208a18dbc5c94f6
Christian Scott <christian.s@canva.com>
2020-05-01 16:17:53 +1000

    format

--- main.go
+++ main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "strings"
+	"flag"
+	"fmt"
+	"strings"
 )
 
 func main() {
-  namespace := flag.String("namespace", "", "prefixed to the branch name to namespace it")
-  sep := flag.String("sep", "-", "seperator used to join branch name parts")
-  namespaceSep := flag.String("nsSep", ":", "seperator used to join the namespace and the branch name")
+	sep := flag.String("sep", "-", "seperator used to join branch name parts")
+	namespace := flag.String("namespace", "", "prefixed to the branch name to namespace it")
+	namespaceSep := flag.String("nsSep", ":", "seperator used to join the namespace and the branch name")
 
-  flag.Parse()
+	flag.Parse()
 
-  slugified := strings.ToLower(strings.ReplaceAll(strings.Join(flag.Args(), *sep), " ", *sep))
-  if *namespace != "" {
-    fmt.Println(*namespace + *namespaceSep + slugified)
-  } else {
-    fmt.Println(slugified)
-  }
+	fmt.Println(branchify(flag.Args(), *sep, *namespace, *namespaceSep))
+}
+
+func branchify(args []string, sep, namespace, namespaceSep string) string {
+	slugified := strings.ToLower(strings.ReplaceAll(strings.Join(flag.Args(), sep), " ", sep))
+	if namespace == "" {
+		return slugified
+	}
+	return namespace + namespaceSep + slugified
 }
